27570d3fe0f1cf20de4a04e429347475086d31b8
Average77 <904545301@qq.com>
2017-07-21 08:19:59 +0800

    添加注释以及添加分组列表

--- app/src/main/java/com/isabella/test/MainActivity.java
+++ app/src/main/java/com/isabella/test/MainActivity.java
@@ -1,12 +1,12 @@
 package com.isabella.test;
 
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<String> list = new ArrayList<>();
+    private List<Bean> list = new ArrayList<>();
     private Button bt1;
     private Button bt2;
     private PopupWindow pw;
@@ -26,33 +26,65 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private int i;
     private View view;
+    private int heightPixels;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //弹出simple popupWindow
         bt1 = (Button) findViewById(R.id.button);
+        //弹出悬浮标签popupWindow
         bt2 = (Button) findViewById(R.id.button1);
-        int heightPixels = getResources().getDisplayMetrics().heightPixels;
+        //获取屏幕高度
+        heightPixels = getResources().getDisplayMetrics().heightPixels;
+        //默认设置popupWindow的高度为屏幕的一半(用于simple popupWindow)
         i = heightPixels / 2;
+        //初始化popupWindow
         initPopuWindow();
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setVisibility(View.GONE);
+                //默认listView滑动到顶部
+                //因为布局复用,如果打开 悬浮标签的,滑动到第一个可见条目是6的,那么再打开 simple的时候,也会在第一可见条目为6的地方,这是不合适的
+                lv.setSelection(0);
+                //屏幕高度的二分之一
+                i = heightPixels / 2;
+                //设置为popupWindow的高度
+                pw.setHeight(i);
+                //初始化simple数据
+                init();
+                //设置 最上方显示的 悬浮标签为隐藏状态
+                if (textView.getVisibility() != View.GONE) {
+                    textView.setVisibility(View.GONE);
+                }
+                //设置view的背景 (圆角)
                 view.setBackgroundResource(R.drawable.sp_popup_1);
-                pw.showAtLocation(MainActivity.this.findViewById(R.id.line1), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                //展示在界面上, 在屏幕最下方
+                pw.showAtLocation(MainActivity.this.findViewById(R.id.line1), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
             }
         });
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setVisibility(View.VISIBLE);
+                lv.setSelection(0);
+                //屏幕高度的五分之三
+                i = heightPixels * 3 / 5;
+                pw.setHeight(i);
+                //初始化悬浮标签的数据
+                initData();
+                //默认为显示状态
+                if (textView.getVisibility() != View.VISIBLE) {
+                    textView.setVisibility(View.VISIBLE);
+                }
                 view.setBackgroundResource(R.drawable.sp_popup);
-                pw.showAtLocation(MainActivity.this.findViewById(R.id.line1), Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+                pw.showAtLocation(MainActivity.this.findViewById(R.id.line1), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
             }
         });
     }
+
+    List<Integer> intList = new ArrayList<>();
 
     private void initPopuWindow() {
         /**
@@ -66,18 +98,19 @@ public class MainActivity extends AppCompatActivity {
         view.setBackgroundResource(R.drawable.sp_popup);
         textView = (TextView) view.findViewById(R.id.textview);
         Button pope_bt = (Button) view.findViewById(R.id.popup_bt);
-        ListView lv = (ListView) view.findViewById(R.id.lv);
+        lv = (ListView) view.findViewById(R.id.lv);
 
-        initData();
+
         adapter = new MyAdapter(this, list);
         lv.setAdapter(adapter);
 
         //创建PopupWindow
 
         pw = new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, i);
-        pw.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#efefef")));
+        pw.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.popup_color)));
         //点击PopupWindow的外部消失PopupWindow
         pw.setOutsideTouchable(true);
+        //设置动画
         pw.setAnimationStyle(R.style.Animation);
         //点击返回键让PopupWindow消失
         pw.setFocusable(true);
@@ -92,18 +125,97 @@ public class MainActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //获取当前点击条目
                 Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
                 pw.dismiss();
+            }
+        });
+        //滑动监听
+        lv.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                //当总条目不为0的时候说明有数据
+                if (totalItemCount != 0) {
+                    //当传入的标签名不为null说明是悬浮标签的popupWindow,走下面的代码块
+                    if (list.get(firstVisibleItem).getGroup() != null) {
+                        //设置为当前标签名
+                        textView.setText(list.get(firstVisibleItem).getGroup());
+                        //当时第一条时
+                        if (list.get(firstVisibleItem).isTemp()) {
+                            // textView.setVisibility(View.GONE);
+                            //判断是否是第一条的数据,因为下方改变了temp的值
+                            //所以需要这个值来判断
+                            //用于在数据倒着滑的时候显示第一条的标签
+                            for (int i1 = 0; i1 < intList.size(); i1++) {
+                                if (i1 == firstVisibleItem) {
+                                    list.get(firstVisibleItem).setTemp(true);
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        } else {
+                            //用于数据在正着滑的时候隐藏标签
+                            if (textView.getVisibility() != View.VISIBLE) {
+                                textView.setVisibility(View.VISIBLE);
+                            }
+                            //把是true的position添加到iniList里面,方便返回去的时候查询使用
+                            intList.add(firstVisibleItem);
+                            //把temp设置为false  因为这里变成false,没有判断的依据了.所以需要添加一个新的标记 iniList
+                            list.get(firstVisibleItem).setTemp(false);
+                            adapter.notifyDataSetChanged();
+                            //  textView.setText(list.get(firstVisibleItem).getGroup());
+                        }
+                    }
+                }
+
             }
         });
 
 
     }
 
+    //初始化有悬浮标签 popupwindow listview数据
     private void initData() {
-        for (int i = 0; i < 10; i++) {
-            list.add("Test"+i);
+        //先清空
+        list.clear();
+        //模拟数据
+        //设置为true表示为每个相同标签的第一条数据
+        // 三个参数依次为 条目显示的内容 条目标签内容 是否为该标签的第一条数据
+        for (int i = 0; i < 5; i++) {
+            list.add(new Bean("Test" + i, "投诉", false));
         }
+        list.add(new Bean("Test" + 5, "意见", true));
+        for (int i = 6; i < 13; i++) {
+            list.add(new Bean("Test" + i, "意见", false));
+        }
+        list.add(new Bean("Test" + 13, "举报", true));
+        for (int i = 14; i < 20; i++) {
+            list.add(new Bean("Test" + i, "举报", false));
+        }
+        //必须刷新
+        adapter.notifyDataSetChanged();
     }
 
+    //初始化 simple popupwindow listview 数据
+    private void init() {
+        //必须先清空,因为用的是同一个集合,如果不清空会造成数据的重复  如果不需要复用布局,可以写两个集合,就不需要清空了
+        list.clear();
+        for (int i = 0; i < 10; i++) {
+            list.add(new Bean("Test" + i, null, false));
+        }
+        //必须刷新 ,因为复用布局和设置popupwindow属性,所以需要先设置适配器再添加数据,所以需要刷新一下,(网络请求同理)注意adapter中返回的count值
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (pw != null) {
+            pw = null;
+        }
+    }
 }
--- app/src/main/java/com/isabella/test/MyAdapter.java
+++ app/src/main/java/com/isabella/test/MyAdapter.java
@@ -15,16 +15,17 @@ import java.util.List;
 
 public class MyAdapter extends BaseAdapter {
     Context context;
-    List<String> list;
+    List<Bean> list;
 
-    public MyAdapter(Context context, List<String> list) {
+    public MyAdapter(Context context, List<Bean> list) {
         this.context = context;
         this.list = list;
+
     }
 
     @Override
     public int getCount() {
-        return list!=null?list.size():0;
+        return list != null ? list.size() : 0;
     }
 
     @Override
@@ -40,18 +41,40 @@ public class MyAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-        if (convertView==null){
-            holder=new ViewHolder();
-            convertView=View.inflate(context,R.layout.popup_item,null);
-            holder.tv= (TextView) convertView.findViewById(R.id.tv);
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = View.inflate(context, R.layout.popup_item, null);
+            holder.tv = (TextView) convertView.findViewById(R.id.tv);
+            holder.item = (TextView) convertView.findViewById(R.id.item_tv);
             convertView.setTag(holder);
-        }else{
-            holder= (ViewHolder) convertView.getTag();
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
-        holder.tv.setText(list.get(position));
+        holder.tv.setText(list.get(position).getContent());
+        //如果group不为空说明是悬浮标签的popupWindow
+        if (list.get(position).getGroup() != null) {
+            //标签是组中第一个的时候
+            if (list.get(position).isTemp()) {
+                //设置为显示状态
+                holder.item.setText(list.get(position).getGroup());
+                if (holder.item.getVisibility() != View.VISIBLE) {
+                    holder.item.setVisibility(View.VISIBLE);
+                }
+            } else {
+                if (holder.item.getVisibility() != View.GONE) {
+                    holder.item.setVisibility(View.GONE);
+                }
+            }
+            //否则就说明是simple的popupWindow , 就把item全部隐藏
+        } else {
+            if (holder.item.getVisibility() != View.GONE) {
+                holder.item.setVisibility(View.GONE);
+            }
+        }
         return convertView;
     }
-    static  class ViewHolder{
-        TextView tv;
+
+    static class ViewHolder {
+        TextView tv, item;
     }
 }
